a15abe1ce2ec30403247237b8757585c2f1febce
Eli Mendonça <contato@elimendonca.com>
2022-02-25 11:59:51 -0300

    Ajuste na parte dos arquivos de download

--- src/components/PaFiles.jsx
+++ src/components/PaFiles.jsx
@@ -5,63 +5,57 @@ export function PaFiles(props) {
       <table className="files">
         <thead>
           <tr>
-            <th>Themes: </th>
-            <th>Plugin:</th>
-            <th>WP:</th>
+            <th>Arquivos</th>
+            <th>Categoria</th>
+            <th>Observações</th>
+            <th>Changelog</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/themes/pa-theme-sedes-child.zip"
-              >
-                pa-theme-sedes-child.zip
-              </a>
+            <td><a href="https://files.internetdsa.com/themes/pa-theme-sedes.zip" target="_blank">pa-theme-sedes.zip</a>
             </td>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/plugins/advanced-custom-fields-pro.zip"
-              >
-                advanced-custom-fields-pro.zip
-              </a>
-            </td>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/wp/pt_BR.zip"
-              >
-                pt_BR.zip
-              </a>
-            </td>
+            <td>Tema pai</td>
+            <td>&nbsp;</td>
+            <td><a href="https://github.com/igrejaadventista/pa-theme-sedes/blob/master/CHANGELOG.md" target="_blank">link</a></td>
           </tr>
           <tr>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/themes/pa-theme-sedes.zip"
-              >
-                pa-theme-sedes.zip
-              </a>
-            </td>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/plugins/cloudflare.zip"
-              >
-                cloudflare.zip
-              </a>
-            </td>
-            <td>
-              <a
-                target="_blank"
-                href="https://files.internetdsa.com/wp/es_ES.zip"
-              >
-                es_ES.zip
-              </a>
-            </td>
+            <td><a href="https://files.internetdsa.com/themes/pa-theme-sedes-child.zip"
+              target="_blank">pa-theme-sedes-child.zip</a></td>
+            <td>Tema filho</td>
+            <td>Depende do tema pai</td>
+            <td><a href="https://github.com/igrejaadventista/pa-theme-sedes-child/blob/master/CHANGELOG.md" target="_blank">link</a></td>
+          </tr>
+          <tr>
+            <td><a href="https://files.internetdsa.com/plugins/advanced-custom-fields-pro.zip"
+              target="_blank">advanced-custom-fields-pro.zip</a></td>
+            <td>Plugin</td>
+            <td>Obrigat&oacute;rio</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="https://files.internetdsa.com/plugins/cloudflare.zip" target="_blank">cloudflare.zip</a></td>
+            <td>Plugin</td>
+            <td>Opcional</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="https://files.internetdsa.com/wp/pt_BR.zip" target="_blank">pt_BR.zip</a></td>
+            <td>Bundle</td>
+            <td>Instala&ccedil;&atilde;o completa em PT</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="https://files.internetdsa.com/wp/es_ES.zip" target="_blank">es_ES.zip</a></td>
+            <td>Bundle</td>
+            <td>Instala&ccedil;&atilde;o completa em ES</td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><a href="https://www.dropbox.com/s/spu46l5hqucmer4/Template_Slider_PA.psd?dl=0" target="_blank">Slider</a></td>
+            <td>Assets</td>
+            <td>Modelo do slider&nbsp;</td>
+            <td>&nbsp;</td>
           </tr>
         </tbody>
       </table>
